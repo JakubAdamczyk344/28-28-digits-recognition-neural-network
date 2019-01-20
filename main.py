fd3d4dbd17b877888ae4d4d4a6133b581f77295a
Jakub Adamczyk <jakub.adamczyk344@gmail.com>
2019-01-20 20:14:38 +0100

    Fixed feedFordward func, added data loading using mnistLoader

--- main.py
+++ main.py
@@ -1,20 +1,22 @@
 #Neural network have changeable number of hidden layers and number of neurons in each hidden layer
 # These vars are provided as a vector, each value stores number of neurons in each layer
 #There will be 784 inputs (one for each pixel) and 10 outputs (one for each digit)
-#Activation fucntion will be sigmoid
+#Activation function will be sigmoid
 #Weight and biases will be tuned using backpropagation algorithm
-#We can change size of batches and number of epochs
 
 import numpy as np
+import mnistLoader
 
 class NeuralNetwork:
 
     numOfInputs = 784
     numOfOutputs = 10
+    numOfExamplesInBatch = 10
+    numOfEpochs = 100
 
-    def __init__(self, hiddenLayers, numOfExamplesInBatch, numOfEpochs):
+    def __init__(self, hiddenLayers):
 
-        #self.inputs = ["tu trzeba zaciągać dane, znajdź przykład w necie"]
+        self.trainigData, self.validationData, self.testData = mnistLoader.loadDataWrapper
 
         #Initializing biases and weights for each neuron in hidden layers
         self.biases = [np.random.rand(x, 1) for x in hiddenLayers] + [np.random.rand(self.numOfOutputs,1)]
@@ -23,7 +25,7 @@ class NeuralNetwork:
     def sigmoid(self, x):
         return 1.0/(1.0 + np.exp(-x))
 
-    def feedForward(self):
-        output = self.inputs
+    def feedForward(self, inputs):
+        output = inputs
         for x in range(len(self.weights)):
-            output = np.dot(self.weights[x], output) + self.biases[x]
+            output = self.sigmoid(np.dot(self.weights[x], output) + self.biases[x])
